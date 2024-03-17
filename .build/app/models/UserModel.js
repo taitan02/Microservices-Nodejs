24a689657da23d7e0988a49979dd6049cf8a9366
taint <tai.nguyen0299@gmail.com>
2024-03-18 01:34:25 +0700

    fix: stop tracking .build directory

--- .build/app/models/UserModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserType = void 0;
-var UserType;
-(function (UserType) {
-    UserType["BUYER"] = "BUYER";
-    UserType["SELLER"] = "SELLER";
-})(UserType || (exports.UserType = UserType = {}));
-//# sourceMappingURL=UserModel.js.map
